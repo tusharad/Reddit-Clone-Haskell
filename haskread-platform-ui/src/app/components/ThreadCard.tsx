0640ca7ae6f3ffafe74bfef95d61fc8754b5cb73
tusharad <tusharadhatrao@gmail.com>
2024-12-14 14:51:06 +0530

    Refactored ThreadCard

--- haskread-platform-ui/src/app/components/ThreadCard.tsx
+++ haskread-platform-ui/src/app/components/ThreadCard.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faComment, faShare, faThumbsDown as solidThumbsDown, faThumbsUp as solidThumbsUp } from '@fortawesome/free-solid-svg-icons'
-import { faThumbsUp as regularThumbsup, faThumbsDown as regularThumbsDown } from '@fortawesome/free-regular-svg-icons'
-import { useEffect, useState } from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faComment, faShare, faThumbsDown as solidThumbsDown, faThumbsUp as solidThumbsUp} from '@fortawesome/free-solid-svg-icons'
+import {faThumbsUp as regularThumbsup, faThumbsDown as regularThumbsDown} from '@fortawesome/free-regular-svg-icons'
+import {useEffect, useState} from 'react';
 
 
 interface ThreadCardProps {
@@ -19,19 +19,8 @@ interface ThreadCardProps {
     userPostReaction: number;
 }
 
-const getUpIcon = (val : number) => {
-    if (val == 1)
-        return solidThumbsUp
-    else
-        return regularThumbsup
-}
-
-const getDownIcon = (val : number) => {
-    if (val == 2)
-        return solidThumbsDown 
-    else
-        return regularThumbsDown 
-}
+const getUpIcon = (val: number) => {return val == 1 ? solidThumbsUp : regularThumbsup}
+const getDownIcon = (val: number) => {return val == 2 ? solidThumbsDown : regularThumbsDown}
 
 const ThreadCard: React.FC<ThreadCardProps> = ({
     title,
@@ -45,116 +34,79 @@ const ThreadCard: React.FC<ThreadCardProps> = ({
     threadIDForThreadInfo,
     userPostReaction,
 }) => {
-    
+
     const [upvoteC, setUpvoteC] = useState(0);
     const [downvoteC, setDownvoteC] = useState(0);
     const [userUpvoteReaction, setUserUpvoteReaction] = useState(regularThumbsup);
     const [userDownvoteReaction, setUserDownvoteReaction] = useState(regularThumbsDown);
 
     useEffect(() => {
-        const setData = async () => {
-          if(upvoteCount)
-            setUpvoteC(upvoteCount);
-          if(downvoteCount)
-            setDownvoteC(downvoteCount);
-          setUserUpvoteReaction(getUpIcon(userPostReaction));
-          setUserDownvoteReaction(getDownIcon(userPostReaction));
+        const setData = () => {
+            if (upvoteCount) setUpvoteC(upvoteCount);
+            if (downvoteCount) setDownvoteC(downvoteCount);
+            setUserUpvoteReaction(getUpIcon(userPostReaction));
+            setUserDownvoteReaction(getDownIcon(userPostReaction));
         };
         setData();
-      },[])
+    }, [])
 
-    const updateUserReactions = async (isUpvotted : boolean) => {
-        if(isUpvotted) {
+    const updateUserReactions = async (isUpvotted: boolean) => {
+        if (isUpvotted) {
             // user upvoted the comment
             // if user had already upvoted, then decrease the upvote count and change thumps up to regular
             // else check if user had downvoted the post, if yes then decrease the downvote count and change thumps down to regular
             // and increase the upvote count and change the thumps up to solid.
-          if (userUpvoteReaction == solidThumbsUp){
-            console.log("removing upvote");
-            setUpvoteC(upvoteC-1);
-            setUserUpvoteReaction(regularThumbsup);
-          }else if(userDownvoteReaction == solidThumbsDown) {
-           console.log("upvote is happening, removing downvote");
-           setDownvoteC(downvoteC-1);
-           setUserDownvoteReaction(regularThumbsDown);
-           setUpvoteC(upvoteC+1);
-           setUserUpvoteReaction(solidThumbsUp);
-          }else{
-            console.log("simple upvote is happening");
-            setUpvoteC(upvoteC+1);
-            setUserUpvoteReaction(solidThumbsUp);
-          }
+            if (userUpvoteReaction == solidThumbsUp) {
+                console.log("removing upvote");
+                setUpvoteC(upvoteC - 1); setUserUpvoteReaction(regularThumbsup);
+            } else {
+                console.log("upvote is happening");
+                if (userDownvoteReaction == solidThumbsDown) {
+                    console.log("removing downvote");
+                    setDownvoteC(downvoteC - 1); setUserDownvoteReaction(regularThumbsDown);
+                }
+                setUpvoteC(upvoteC + 1); setUserUpvoteReaction(solidThumbsUp);
+            }
         }
         else {
-            if(userDownvoteReaction == solidThumbsDown){
+            if (userDownvoteReaction == solidThumbsDown) {
                 console.log("removing downvote");
-                setDownvoteC(downvoteC-1);
-                setUserDownvoteReaction(regularThumbsDown);
-            }else if(userUpvoteReaction == solidThumbsUp) {
-                console.log("downvote is happening, removing upvotes");
-                setUpvoteC(upvoteC - 1);
-                setUserUpvoteReaction(regularThumbsup);
-                setDownvoteC(downvoteC+1);
-                setUserDownvoteReaction(solidThumbsDown);
-            }else{
-                console.log("simple downvote is happening");
-                setDownvoteC(downvoteC+1);
-                setUserDownvoteReaction(solidThumbsDown);
+                setDownvoteC(downvoteC - 1); setUserDownvoteReaction(regularThumbsDown);
+            } else {
+                console.log("downvote is happening");
+                if (userUpvoteReaction == solidThumbsUp) {
+                    setUpvoteC(upvoteC - 1); setUserUpvoteReaction(regularThumbsup);
+                }
+                setDownvoteC(downvoteC + 1); setUserDownvoteReaction(solidThumbsDown);
             }
         }
     }
 
-    const handleUpvote = async () => {
+    const handleVote = async (isUpvote: boolean) => {
         const token = localStorage.getItem('jwt_token');
         if (!token) {
-          console.log('User is not authenticated');
-          return;
+            console.log('User is not authenticated');
+            return;
         }
+        const endpoint = isUpvote ? 'upvote' : 'downvote';
         try {
-          const response = await fetch(`http://localhost:8085/api/v1/user/thread/upvote/${threadIDForThreadInfo}`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${token}`,
-            },
-          });
-    
-          if (response.ok) {
-            updateUserReactions(true);
-          } else {
-            const res = await response.text();
-            console.log(res);
-          }
-        } catch (error) {
-          console.log('An error occurred: ' + error);
-        }
-      };
+            const response = await fetch(`http://localhost:8085/api/v1/user/thread/${endpoint}/${threadIDForThreadInfo}`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                },
+            });
 
-      const handleDownvote = async () => {
-        const token = localStorage.getItem('jwt_token');
-        if (!token) {
-          console.log('User is not authenticated');
-          return;
-        }
-        try {
-          const response = await fetch(`http://localhost:8085/api/v1/user/thread/downvote/${threadIDForThreadInfo}`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              Authorization: `Bearer ${token}`,
-            },
-          });
-    
-          if (response.ok) {
-            updateUserReactions(false);
-          } else {
-            const res = await response.text();
-            console.log(res);
-          }
+            if (response.ok) {
+                updateUserReactions(isUpvote);
+            } else {
+                console.log(await response.text());
+            }
         } catch (error) {
-          console.log('An error occurred: ' + error);
+            console.log('An error occurred: ' + error);
         }
-      };
+    }
 
     return (
         <div className="card-bg shadow-lg rounded-lg mb-6 overflow-hidden">
@@ -184,12 +136,12 @@ const ThreadCard: React.FC<ThreadCardProps> = ({
             )}
             <div className="flex justify-between items-center p-4 border-t">
                 <div className="flex space-x-2 items-center">
-                    <button className="flex items-center space-x-1 hover:text-blue-500" onClick={handleUpvote}>
-                        <FontAwesomeIcon icon={ userUpvoteReaction } />
+                    <button className="flex items-center space-x-1 hover:text-blue-500" onClick={() => handleVote(true)}>
+                        <FontAwesomeIcon icon={userUpvoteReaction} />
                         <span>{upvoteC}</span>
                     </button>
-                    <button className="flex items-center space-x-1 hover:text-red-500" onClick={handleDownvote}>
-                        <FontAwesomeIcon icon={ userDownvoteReaction } />
+                    <button className="flex items-center space-x-1 hover:text-red-500" onClick={() => handleVote(false)}>
+                        <FontAwesomeIcon icon={userDownvoteReaction} />
                         <span>{downvoteC}</span>
                     </button>
                     <span className="flex items-center space-x-1 ">
